572572d34f91347be34ad3b2da35e3eda5ad0c38
jak010 <bluetoon@naver.com>
2023-01-21 21:33:28 +0900

    save

---
+++ src/tests/settings/local.py
@@ -0,0 +1,23 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'home_ledger',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '9901',
+        'OPTIONS': {
+            'charset': 'utf8mb4'
+        },
+        # 'TEST': { # 테스트를 django test로 할 거면 주석 해제 후 사용
+        #     'CHARSET': 'utf8',
+        #     'COLLATION': 'utf8_general_ci',
+        # }
+    }
+}
