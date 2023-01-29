166cca6ebe4f73e884214b2ed80a25aec6b7325b
jak010 <bluetoon@naver.com>
2023-01-29 14:14:33 +0900

    <add> e2e 테스트 추가 - fix, test_accountbooks_history_delete.py

---
+++ tests/e2e/test_accountbooks_history/test_accountbooks_history_delete.py
@@ -0,0 +1,43 @@
+import pytest
+
+from django.shortcuts import reverse
+
+from application.service import accountbook_service
+
+
+@pytest.fixture
+def accountbook(member_on):
+    return accountbook_service.create_account_book(
+        name="test",
+        description="test",
+        member=member_on
+    )
+
+
+@pytest.fixture
+def accountbook_history(member_on, accountbook):
+    return accountbook_service.create_account_book_history(
+        amount=1000,
+        memo="test",
+        account_book=accountbook
+    )
+
+
+@pytest.mark.django_db
+def test_accountsbooks_history_delete_api(client, auth_header, accountbook, accountbook_history):
+    accountbooks_history_update_view = reverse("application:accountbook_history_detail", kwargs={
+        'accountbook_id': accountbook.reference_id,
+        'accountbook_history_id': accountbook_history.reference_id
+    })
+
+    response = client.delete(
+        accountbooks_history_update_view,
+        data={
+            'amount': 1000,
+            'memo': "test accountbooks history",
+        },
+        content_type="application/json",  # HACK: 처리 안해주면 415 Status 발생
+        **auth_header
+    )
+
+    assert response.status_code == 200
